ec83a3d7336cc041b4f4a71d2fb72143beca2c44
louis-iut <32872097+louis-iut@users.noreply.github.com>
2017-12-08 09:45:20 +0100

    test "failed test task"

--- app/src/androidTest/java/fr/iut/iem/marvelcomics/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/fr/iut/iem/marvelcomics/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("fr.iut.iem.marvelcomics", appContext.getPackageName());
+        assertEquals("fr.iut.iem.marvelcomic", appContext.getPackageName());
     }
 }
--- app/src/main/java/fr/iut/iem/marvelcomics/data/model/Comics.java
+++ app/src/main/java/fr/iut/iem/marvelcomics/data/model/Comics.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 
 public class Comics {
-    
+
 
     private int id;
     private String title;
