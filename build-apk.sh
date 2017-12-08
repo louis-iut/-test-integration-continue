67d41301ec99c8522a1d7280ae16abc747d81b01
louis-iut <32872097+louis-iut@users.noreply.github.com>
2017-12-08 17:47:54 +0100

    no message

--- build-apk.sh
+++ build-apk.sh
@@ -5,10 +5,10 @@ chmod +x gradlew
 
 ./gradlew assembleRelease
 
-chemin='./artifacts'
-mkdir -p $chemin
-name="Comics_Release.apk"
-link=$chemin'/'$name
+#chemin='./artifacts'
+#mkdir -p $chemin
+#name="Comics_Release.apk"
+#link=$chemin'/'$name
 #mv ./app/build/outputs/apk/release/app-release-unsigned.apk $link
 
 #if [[ "$TRAVIS_PULL_REQUEST" != "true" ]]
