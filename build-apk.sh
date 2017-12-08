44b94bdf0f61455869073ae2ac936407af3192f2
louis-iut <32872097+louis-iut@users.noreply.github.com>
2017-12-08 17:14:25 +0100

    no message

--- build-apk.sh
+++ build-apk.sh
@@ -9,7 +9,7 @@ chemin='./artifacts'
 mkdir -p $chemin
 name="Comics_Release.apk"
 link=$chemin'/'$name
-mv ./app/build/outputs/apk/release/app-release-unsigned.apk $link
+#mv ./app/build/outputs/apk/release/app-release-unsigned.apk $link
 
 #if [[ "$TRAVIS_PULL_REQUEST" != "true" ]]
 #then
